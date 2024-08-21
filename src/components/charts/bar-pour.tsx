68354cbc8c6fe7e86a103e83b4b2db4078aab04d
mikkurogue <michael.lindemans@outlook.com>
2024-08-21 15:18:55 +0200

    add comment quick

--- src/components/charts/bar-pour.tsx
+++ src/components/charts/bar-pour.tsx
@@ -9,6 +9,13 @@ type BarPourProps = {
 };
 
 export default function BarPour({ data, options, style }: BarPourProps) {
+  // NOTE: here we just have a simple wrapper for our echart component. As all echarts charts have the same base
+  // we can use it to share functionality (like rendering, events etc) as an abstraction
+  // this current component is meant to only be the bow ontop making the Echart component render the chart that we need
+  // meaning, these types of components should do absolutely NOTHING as we pour all the data into it
+  // The only thing this component does is render the chart with some default options for the chart options required
+  // in this instance it is for a typical normal Bar chart.
+
   return (
     <Echart
       style={{
