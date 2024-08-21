3d4c74eb8713673c8d85399260830ef5cb1b0710
mikkurogue <michael.lindemans@outlook.com>
2024-08-21 21:01:06 +0200

    make fetchdata generic with output by injecting a type

--- src/lib/fetchData.ts
+++ src/lib/fetchData.ts
@@ -1,9 +1,9 @@
-async function fetchData(url: string, endpoint: string) {
+async function fetchData<T>(url: string, endpoint: string) {
   const result = await fetch(`${url}/${endpoint}`);
 
   const data = result.json();
 
-  return data;
+  return data as T;
 }
 
 export { fetchData };
